91d987214c81bc9fcd841347d58524b1440345b3
nnyskr <nnyskr@icloud.com>
2023-09-04 01:27:42 +0200

    wrap filters

--- components/filters-chips.tsx
+++ components/filters-chips.tsx
@@ -1,14 +1,14 @@
-'use client';
-import { useState, useTransition } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
-import { Category } from '@/types';
+"use client";
+import { useState, useTransition } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { Category } from "@/types";
 
 export default function Filters({
   categories,
   currentActiveCategories,
 }: {
   categories: Category[];
-  currentActiveCategories: Category['name'][];
+  currentActiveCategories: Category["name"][];
 }) {
   const router = useRouter();
   const pathname = usePathname();
@@ -35,15 +35,15 @@ export default function Filters({
 
     const categoriesParam = Object.keys(newActiveCategoriesByName)
       .filter((key) => newActiveCategoriesByName[key])
-      .join(',');
+      .join(",");
 
     if (categoriesParam) {
-      searchParams.set('categories', categoriesParam);
+      searchParams.set("categories", categoriesParam);
     } else {
-      searchParams.delete('categories');
+      searchParams.delete("categories");
     }
 
-    searchParams.delete('page');
+    searchParams.delete("page");
 
     startTransition(() => {
       router.replace(`${pathname}?${searchParams.toString()}`);
@@ -51,19 +51,19 @@ export default function Filters({
   }
 
   return (
-    <div className="flex gap-x-4 px-12 mt-8 items-center">
+    <div className="flex flex-wrap gap-2 px-8 mt-8 items-center">
       {categories.map((category) => (
         <div
           key={category.id}
           onClick={() => onCategoryClick(category.name)}
           className={`p-2 rounded cursor-pointer border ${
-            activeCategoriesByName[category.name] ? 'bg-sky-500' : ''
+            activeCategoriesByName[category.name] ? "bg-sky-500" : ""
           }`}
         >
           {category.name}
         </div>
       ))}
-      {isPending && <div className={'animate-spin'}>🔄</div>}
+      {isPending && <div className={"animate-spin"}>🔄</div>}
     </div>
   );
 }
--- components/filters.tsx
+++ components/filters.tsx
@@ -1,21 +1,19 @@
-import prisma from '@/lib/prisma';
-import FiltersChips from '@/components/filters-chips';
-import { SearchParams } from '@/types';
+import prisma from "@/lib/prisma";
+import FiltersChips from "@/components/filters-chips";
+import { SearchParams } from "@/types";
 
 export default async function Filters({
   searchParams,
 }: {
-  searchParams: Pick<SearchParams, 'categories'>;
+  searchParams: Pick<SearchParams, "categories">;
 }) {
   const categories = await prisma.categories.findMany({});
-  const currentActiveCategories = searchParams.categories?.split(',') || [];
+  const currentActiveCategories = searchParams.categories?.split(",") || [];
 
   return (
-    <div className="flex gap-x-4 px-12">
-      <FiltersChips
-        categories={categories}
-        currentActiveCategories={currentActiveCategories}
-      />
-    </div>
+    <FiltersChips
+      categories={categories}
+      currentActiveCategories={currentActiveCategories}
+    />
   );
 }
